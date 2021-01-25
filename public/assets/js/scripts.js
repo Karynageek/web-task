1d8b6a066297ada7620c81da70fbb662c2d650a8
Vovina-Vredina <77749691+Vovina-Vredina@users.noreply.github.com>
2021-01-25 22:16:23 +0200

    adaptive

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -8,23 +8,27 @@ $(function () {
         });
     });
 
-    $(".menu_item a, .menu_button").click(function () {
+    $(".menu_item a, .menu_button1, .menu_button2").click(function(){
         var elem = $(this).attr("href");
         var dist = $(elem).offset().top;
         var menuHeight = $(".main_header").innerHeight();
 
-        $("html, body").animate({ scrollTop: dist - menuHeight }, 1000);
-        $(".menu_item a").removeClass("active");
-        $(this).addClass("active");
+        $("html, body").animate({"scrollTop": dist - menuHeight}, 1000);
+        if(this.tagName == "A"){
+            $(".menu_item").removeClass("active");
+            $(this).parent().addClass("active");
+        }
         return false;
     });
 
     var clickAmount = 0;
-    $(".price_button").click(function () {
+    $(".price_button").click(function(){
         clickAmount++;
-        if (clickAmount % 2 == 0) $(".table").addClass("hide");
-        else $(".table").removeClass("hide");
-
+        if(clickAmount %2 == 0)
+            $(".table").addClass("hide");
+        else
+            $(".table").removeClass("hide");
+        
         return false;
     });
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
-        <title>Luxury Restaurant</title>
+        <title>BUCKWHEAT ORION</title>
+        <link rel="shortcut icon" href="{{ URL::asset('assets/img/logo2.png') }}" type="image/png">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700|Sintony:400,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
     </head>
     <body>
@@ -14,41 +16,49 @@
             <div class="main_header">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-3">
+                        <div class="col-3 col-md-4 col-lg-3">
                             <img src="{{ URL::asset('assets/img/logo2.png') }}" class="logo" alt="Luxury buckwheat">
                         </div>
-                        <div class="col-lg-5">
-                            <nav>
-                                <ul class="menu d-flex justify-content-between">
-                                    <li class="menu_item">
-                                        <a href="#header" class="active">
-                                            MAIN
-                                        </a>
-                                    </li>
-                                    <li class="menu_item ">
-                                        <a href="#about">
-                                            ABOUT BUCKWHEAT
-                                        </a>
-                                    </li>
-                                    <li class="menu_item">
-                                        <a href="#special">
-                                            PRICE
-                                        </a>
-                                    </li>
-                                    <li class="menu_item">
-                                        <a href="#footer">
-                                            CONTACTS
-                                        </a>
-                                    </li>
-                                </ul>
-                            </nav>
+                        <div class="col-6 col-md-4 col-lg-6 d-flex justify-content-between">                        
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav menu d-flex justify-content-between">
+                                <li class="nav-item menu_item active">
+                                    <a href="#header">
+                                        MAIN
+                                    </a>
+                                </li>
+                                <li class="nav-item menu_item">
+                                    <a href="#about">
+                                        ABOUT BUCKWHEAT
+                                    </a>
+                                </li>
+                                <li class="nav-item menu_item">
+                                    <a href="#special">
+                                        PRICE
+                                    </a>
+                                </li>
+                                <li class="nav-item menu_item">
+                                    <a href="#footer">
+                                        CONTACTS
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
-                        <div class="col-lg-4">
-                            <button href="#compare" class="menu_button cta d-flex justify-content-center float-right">
-                                <span class="grn_icon"> </span>
-                                COMPARE PRICES
+                        </nav>                      
+                    </div>
+                        <div class="col-3 col-md-4 col-lg-3">
+                            <button href="#compare" class="menu_button1 cta d-flex justify-content-center float-right">
+                            <span class="grn_icon"> </span>
+                            COMPARE PRICES
                             </button>
-                        </div>
+                            <button href="#compare" class="menu_button2 cta d-flex justify-content-center float-right">
+                            <span class="grn_icon"> </span>
+                            </button>
+                    </div>
                     </div>
                 </div>	
             </div>
@@ -59,7 +69,7 @@
                         <div class="col-lg-12 float-right">
                             <div class="title">
                                 <h1 class="title_heading">
-                                    From <span>buckwheat</span> and millet <br>and porridge and paste
+                                    From <span>buckwheat</span> and millet<!--  <br> -->and porridge and paste
                                 </h1>
                                 <p class="title_desc">
                                     © Ukrainian proverb
@@ -88,16 +98,16 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3">
+                    <div class="col-6 col-lg-3 d-flex justify-content-center">
                         <div class="food-images food-images-down">
-                            <img src="{{ URL::asset('assets/img/food1.jpg') }}" alt="Pasta" class="food-images_item">
-                            <img src="{{ URL::asset('assets/img/food2.jpg') }}" alt="Chicken" class="food-images_item">
+                            <img src="{{ URL::asset('assets/img/food1.jpg') }}" alt="Pasta" class="food-images_item img-fluid">
+                            <img src="{{ URL::asset('assets/img/food2.jpg') }}" alt="Chicken" class="food-images_item img-fluid">
                         </div>
                     </div>
-                    <div class="col-lg-3">
+                    <div class="col-6 col-lg-3 d-flex justify-content-center">
                         <div class="food-images">
-                            <img src="{{ URL::asset('assets/img/food3.jpg') }}" alt="Meat" class="food-images_item">
-                            <img src="{{ URL::asset('assets/img/food4.jpg') }}" alt="Fish" class="food-images_item">
+                            <img src="{{ URL::asset('assets/img/food3.jpg') }}" alt="Meat" class="food-images_item img-fluid">
+                            <img src="{{ URL::asset('assets/img/food4.jpg') }}" alt="Fish" class="food-images_item img-fluid">
                         </div>
                     </div>
                 </div>
@@ -114,66 +124,60 @@
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-lg-4">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-lg-4 col-md-6 col-sm-10">
                         <div class="food">
                             <img src="{{ URL::asset('assets/img/novus2.jpg') }}" alt="shop1" class="food_img img-fluid">
                             <h3 class="food_title">
                                 Novus
                             </h3>
-                            <p class="food_desc">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.  
+                            <p class="food_desc novus">
+                                «NOVUS. Trustworthy» 
                             </p>
                             <div class="food_details d-flex">
                                 <div class="food_del">
-                                    Home <span>Delivery</span>
+                                    Contact <span>Сenter</span>
                                 </div>
                                 <div class="food_phone">
-                                    1-008 005 006
+                                    0-800-601-729
                                 </div>
-                                <!-- <div class="food_price yellow ml-auto">
-                                        <sup><small>$</small></sup>89.00
-                                </div> -->
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-sm-10 col-md-6 col-lg-4">
                         <div class="food">
                             <img src="{{ URL::asset('assets/img/eco2.jpg') }}" alt="shop2" class="food_img img-fluid">
                             <h3 class="food_title">
                                 Eco Market
                             </h3>
                             <p class="food_desc">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.  
+                                «Eco Market» is an economical supermarket! 
                             </p>
                             <div class="food_details d-flex">
                                 <div class="food_del">
-                                    Home <span>Delivery</span>
+                                    Contact <span>Сenter</span>
                                 </div>
                                 <div class="food_phone">
-                                    1-008 005 006
-                                </div>
-                                <!-- <div class="food_price yellow ml-auto">
-                                        <sup><small>$</small></sup>89.00
-                                </div> -->
+                                    0-800-302-055
+                                </div>                             
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4">
+                    <div class="col-sm-10 col-md-6 col-lg-4">
                         <div class="food">
                             <img src="{{ URL::asset('assets/img/metro2.jpg') }}" alt="shop3" class="food_img img-fluid">
                             <h3 class="food_title">
                                 Metro
                             </h3>
                             <p class="food_desc">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                «Our business is the success of our customers»
                             </p>
                             <div class="food_details d-flex">
                                 <div class="food_del">
-                                    Home <span>Delivery</span>
+                                    Contact <span>Сenter</span>
                                 </div>
                                 <div class="food_phone">
-                                    1-008 005 006
+                                    0-800-501-401
                                 </div>
                             </div>
                         </div>
@@ -181,7 +185,7 @@
                 </div>
 
                 <div class="row d-flex justify-content-between align-items-center control-panel" id="compare">
-                    <div class="control-panel-shops">
+                    <div class="align-items-center control-panel-shops col-12 col-md-3 col-lg-3">
                         <div class="control-panel-shops-checkbox">
                             <input type="checkbox" id="shop-novus" name="selected-novus" value="novus">
                             <label for="shop-novus">Novus</label>
@@ -195,9 +199,11 @@
                             <label for="shop-metro">Metro</label>
                         </div>
                     </div>
-                    <div class="control-panel-search flex-grow-1 d-flex justify-content-center align-items-stretch">
-                        <input type="text" class="control-panel-search__text flex-grow-1" placeholder="Name of product you want to compare price for">
-                        <div class="cta control-panel-search__submit">Compare prices</div>
+                    <div class="control-panel-search flex-grow-1 d-flex justify-content-center align-items-stretch col-12 col-md-9 col-lg-9">
+                        <div class="row">
+                            <input type="text" class=" col-12 control-panel-search__text flex-grow-1" placeholder="Name of product you want to compare price for">
+                            <div class="cta col-12 control-panel-search__submit">Compare prices</div>
+                        </div>
                     </div>
                 </div>
                 <div class="row price-compare__results"></div>
@@ -245,6 +251,7 @@
         <script src="https://code.jquery.com/jquery-3.4.1.js"
                 integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
         crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="{{ URL::asset('assets/js/scripts.js') }}"></script>
     </body>
 </html>
